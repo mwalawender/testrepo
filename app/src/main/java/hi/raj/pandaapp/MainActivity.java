403505efdf6d22bd9859c30fd68918c601879a86
Michal Walawender <mwalwen@gmail.com>
2016-04-19 21:54:45 +0200

    added log utils

--- app/src/main/java/hi/raj/pandaapp/MainActivity.java
+++ app/src/main/java/hi/raj/pandaapp/MainActivity.java
@@ -2,14 +2,23 @@ package hi.raj.pandaapp;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import api.JobRequestAPI;
+import base.BaseActivity;
+import model.JobList;
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+public class MainActivity extends BaseActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +27,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(getString(R.string.base_url)).addConverterFactory(GsonConverterFactory.create()).build();
+        JobRequestAPI jobRequestAPI = retrofit.create(JobRequestAPI.class);
+        Call<JobList> call = jobRequestAPI.loadJobs();
+
+        call.enqueue(new Callback<JobList>() {
+            @Override
+            public void onResponse(Response<JobList> response, Retrofit retrofit) {
+
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
     }
